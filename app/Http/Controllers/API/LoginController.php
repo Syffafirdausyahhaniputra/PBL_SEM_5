3327d7b6318de4cfb2f8aa2fb3f87c97b02d34a3
syffafirdausyahhaniputra <syffafirdausyahhaniputra@gmail.com>
2024-11-12 11:16:59 +0700

    progress api login

---
+++ app/Http/Controllers/API/LoginController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class LoginController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        // Validasi input
+        $validator = Validator::make($request->all(), [
+            'username' => 'required',
+            'password' => 'required'
+        ]);
+
+        // Jika validasi gagal
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        // Ambil kredensial
+        $credentials = $request->only('username', 'password');
+
+        // Cek autentikasi
+        if (!$token = auth()->guard('api')->attempt($credentials)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Username atau Password Anda salah'
+            ], 401);
+        }
+
+        // Ambil data pengguna
+        $user = auth()->guard('api')->user();
+        $roleId = $user->role_id; // Mengambil role_id dari UserModel
+
+        // Respons jika login berhasil
+        return response()->json([
+            'success' => true,
+            'user' => [
+                'id' => $user->id,
+                'username' => $user->username,
+                'role_id' => $roleId
+            ],
+            'token' => $token
+        ], 200);
+    }
+}

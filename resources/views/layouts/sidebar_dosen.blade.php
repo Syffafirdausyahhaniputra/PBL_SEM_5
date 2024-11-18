1f454b5bbe1f445d96c39e3c5bd7caae0e3fc8a4
Maritza Ulfa Huriyah <maritza.ulfa.huriyah@gmail.com>
2024-11-18 13:45:23 +0700

    Progress Dosen

---
+++ resources/views/layouts/sidebar_dosen.blade.php
@@ -0,0 +1,34 @@
+<div class="sidebar">
+    <!-- Sidebar Menu -->
+    <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+            <li class="nav-item">
+                <a href="{{ url('/welcome') }}" class="nav-link {{ $activeMenu == 'dashboard' ? 'active' : '' }} ">
+                    <i class="nav-icon fas fa-tachometer-alt"></i>
+                    <p>Dashboard</p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="{{ url('/riwayat') }}" class="nav-link {{ $activeMenu == 'riwayat' ? 'active' : '' }} ">
+                    <i class="nav-icon fas fa-history"></i>
+                    <p>Riwayat</p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="{{ url('/profile') }}" class="nav-link {{ $activeMenu == 'profile' ? 'active' : '' }} ">
+                    <i class="nav-icon far fa-user"></i>
+                    <p>Profile</p>
+                </a>
+            </li>
+            <br>
+            <!-- Logout Button -->
+            <li class="nav-item">
+                <a href="{{ url('/logout') }}" class="nav-link">
+                    <button class="btn btn-danger btn-block">
+                        <i class="fas fa-sign-out-alt"></i> Logout
+                    </button>
+                </a>
+            </li>
+        </ul>
+    </nav>
+</div>

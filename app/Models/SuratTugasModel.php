9be2d039932ffa21f9ff968377aa58ad50b6dd0c
syffafirdausyahhaniputra <syffafirdausyahhaniputra@gmail.com>
2024-11-21 14:01:26 +0700

    tambahan model

---
+++ app/Models/SuratTugasModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SuratTugasModel extends Model
+{
+    use HasFactory;
+}

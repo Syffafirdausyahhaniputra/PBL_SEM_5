afcdb6785d68d2dc0c276f05e65f7b4297133f74
syffafirdausyahhaniputra <syffafirdausyahhaniputra@gmail.com>
2024-12-08 21:54:38 +0700

    update web

--- routes/web.php
+++ routes/web.php
@@ -42,9 +42,6 @@ Route::get('login', [AuthController::class, 'login'])->name('login');
 Route::post('login', [AuthController::class, 'postlogin']);
 Route::get('logout', [AuthController::class, 'logout'])->middleware('auth');
 
-Route::get('register', [AuthController::class, 'register'])->name('register');
-Route::post('register', [AuthController::class, 'postRegister']);
-
 // Group route yang memerlukan autentikasi
 Route::middleware('auth')->group(function () {
     Route::get('/welcome', [WelcomeController::class, 'index']);

c698a93f8779568d7adf62a9c8a755ec56e7afa5
syffafirdausyahhaniputra <syffafirdausyahhaniputra@gmail.com>
2024-12-11 08:25:06 +0700

    modifikasi pembetulan font

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -11,8 +11,7 @@
     <!-- Untuk mengirimkan token laravel CSRF pada setiap request ajax -->
 
     <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="{{ asset('adminlte/plugins/fontawesome-free/css/all.min.css') }}">
     <!-- DataTables -->

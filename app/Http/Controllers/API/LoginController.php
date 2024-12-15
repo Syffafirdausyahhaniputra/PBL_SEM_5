12b1213b0cda7c93fb3d48c92eea9a57f12ec414
syffafirdausyahhaniputra <syffafirdausyahhaniputra@gmail.com>
2024-12-16 04:25:50 +0700

    update api

--- app/Http/Controllers/API/LoginController.php
+++ app/Http/Controllers/API/LoginController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Validator;
 
 class LoginController extends Controller
 {
-    public function login(Request $request)
+    public function __invoke(Request $request)
     {
         // Validasi input
         $validator = Validator::make($request->all(), [

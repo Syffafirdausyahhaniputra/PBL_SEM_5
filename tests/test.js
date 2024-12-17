7782e8c3ee305b535e6099506f88bc06e59f453e
syffafirdausyahhaniputra <syffafirdausyahhaniputra@gmail.com>
2024-12-17 09:50:01 +0700

    update test

--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,16 @@
-import http from "k6/http";
-import { sleep } from "k6";
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+  stages: [
+    { duration: '20s', target: 200 }, 
+    { duration: '20s', target: 300 }, 
+    { duration: '20s', target: 400 }, 
+  ],
+};
 
 export default function () {
-    http.get("http://103.126.226.59");
-    sleep(1);
+  const res = http.get('http://103.126.226.59'); // Target domain
+  check(res, { 'status was 200': (r) => r.status == 200 });
+  sleep(1);
 }
